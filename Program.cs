73ce5be996c2b6eb02d9efc1ee510a523759d0fd
Jo <Fig@LAPTOP-GKDM7APE>
2020-01-21 02:23:10 -0500

    commit the change

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio2019CSharpGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
